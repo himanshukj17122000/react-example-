f4cfbab5405d46178e59846d1db65b9bdde3e901
hkj4 <hkj4@duke.edu>
2019-07-29 22:50:58 +0530

    First commit

---
+++ app/components/Nav.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import { ThemeConsumer } from '../contexts/theme'
+import {NavLink} from 'react-router-dom'
+
+
+const activeStyle={
+    color:'rgb(187,46,31)'
+}
+
+export default function Nav () {
+  return (
+    <ThemeConsumer>
+      {({ theme, toggletheme }) => (
+        <nav className='row space-between'>
+        <ul className='row nav'>
+          <li>
+              <NavLink 
+              activeStyle={activeStyle}
+              to='/'
+              exact 
+               className='nav-link'> Popular </NavLink>
+          </li>
+          <li>
+          <NavLink
+          activeStyle={activeStyle}
+          to='/battle' className='nav-link'> Battle </NavLink>
+          </li>
+
+        </ul>
+          <button
+            style={{fontSize: 30}}
+            className='btn-clear'
+            onClick={toggletheme}
+          >
+            {theme === 'light' ? '🔦' : '💡'}
+          </button>
+        </nav>
+      )}
+    </ThemeConsumer>
+  )
+}
---
+++ app/components/withHover.js
@@ -0,0 +1,32 @@
+
+import React from 'react'
+
+export default function withHover (Component, propName = 'hovering') {
+  return class WithHover extends React.Component {
+    state = {
+        hovering: false,
+      }
+     mouseOver=()=> {
+      this.setState({
+        hovering: true
+      })
+    }
+    mouseOut=()=> {
+      this.setState({
+        hovering: false
+      })
+    }
+    render () {
+      const props = {
+        [propName]: this.state.hovering,
+        ...this.props
+      }
+
+      return (
+        <div onMouseOver={this.mouseOver} onMouseOut={this.mouseOut}>
+          <Component {...props} />
+        </div>
+      )
+    }
+  }
+}
